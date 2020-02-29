2ee3fbc43aeaba59bb5b2365ea95d85cf5286a61
Simon Smith <simon@smnth.net>
2020-02-29 01:44:27 -0500

    feat: working bens fault command

--- modules/bens_fault.py
+++ modules/bens_fault.py
@@ -1,24 +1,46 @@
 from discord.ext import commands
+import random
+import asyncio
 
-class BensFaultCog(commands.Cog):
+
+class BensFault(commands.Cog):
+    total_channel_id = 630157407955451925
+    reaction_emoji = [
+        '\N{white heavy check mark}',
+        '\N{ok hand sign}',
+        '\N{thumbs up sign}',
+        '\N{rolling on the floor laughing}',
+        '\N{slightly smiling face}',
+        '\N{upside-down face}',
+        '<:thinkrosa:617807419799633993>',
+        '<:sydneychan:595469748758904843>',
+    ]
+
     def __init__(self, bot):
         self.bot = bot
-        self.total_channel_id = 630157407955451925
-        self.reaction_emoji = ':white_check_mark:'
 
-    @commands.command(name='fault_ben', hidden=True)
-    @command.guild_only()
+    @commands.command(name='faultben', hidden=True)
+    @commands.guild_only()
     async def fault_ben(self, ctx):
-        print('bensfault!')
+        await self.set_total(ctx, self.get_total(ctx) + 1)
+        await ctx.message.add_reaction(random.choice(self.reaction_emoji))
 
-        ctx.message.add_reaction(self.reaction_emoji)
+    @commands.command(name='setbenfaults', hidden=True)
+    @commands.is_owner()
+    async def set_faults(self, ctx, total):
+        await self.set_total(ctx, total)
+        await ctx.message.add_reaction('\N{white heavy check mark}')
 
-    @commands.command(name='set_faults', hidden=True)
-    @command.is_owner()
-    async def set_faults(self, ctx, faults):
-        print(f'setting bens faults to {faults}!')
+    async def set_total(self, ctx, total):
+        channel = self.get_channel(ctx)
+        await channel.edit(name = f'Ben\'s Faults: {total}')
 
-        ctx.message.add_reaction(self.reaction_emoji)
+    def get_total(self, ctx):
+        channel = self.get_channel(ctx)
+        return int(channel.name.split(': ', 1)[1])
+
+    def get_channel(self, ctx):
+        return ctx.bot.get_channel(self.total_channel_id)
 
 def setup(bot):
-    bot.add_cog(BensFaultCog(bot))
+    bot.add_cog(BensFault(bot))
